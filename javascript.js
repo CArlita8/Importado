9b31d692b9344c1c8c6963fcd7793fc001dda2f2
CArlita8 <mcarladellaferrera@gmail.com>
2025-02-14 02:00:17 -0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,14 +1,2 @@
 const productosDestacados = document.getElementById('productos-destacados');
 
-function cargarModelos(marca) {
-    modelosContainer.innerHTML = '';
-
-    if (modelos[marca]) {
-        modelos[marca].forEach(modelo => {
-            // ... (crear el HTML del modelo) ...
-        });
-    }
-
-    // Abre el modal después de cargar los modelos
-    $('#modalModelos').modal('show');
-}
